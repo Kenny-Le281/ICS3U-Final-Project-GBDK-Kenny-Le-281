30bb7021b4c8c20948d80199625d9e607a4e2c36
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2023-05-23 14:53:13 +0000

    lesson 1

---
+++ GBDK/lesson-01/main.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Kenny Le All rights reserved
+//
+// Created by: Kenny Le
+// Created on: May 2023
+// This program is the Space Aliens game for the Gameboy
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+void main() {
+    printf("Hello, World!");
+}
